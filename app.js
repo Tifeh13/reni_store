07ee9fdfe47a0bddaa17dbb98d39d2839779274a
Afolabi Boluwatife <afolabibolu15@gmail.com>
2023-05-25 22:58:10 +0100

    thanks

--- app.js
+++ app.js
@@ -35,26 +35,87 @@ document.addEventListener('DOMContentLoaded', () =>{
     }
 });
 
-//serch option
-const searchInput = document.getElementById('search');
 
-const catalogs = document.querySelectorAll('.item-1');
 
-// Add event listener for input event
-searchInput.addEventListener('input', function(event) {
-  const searchTerm = event.target.value.toLowerCase();
 
-  // loop through wears type and display search result
-  catalogs.forEach(function(catalog) {
-    const title = catalog.querySelector('p').textContent.toLowerCase();
 
-    if (title.includes(searchTerm)) {
-      catalog.style.display = 'block';
-    } else {
-      catalog.style.display = 'none';
+
+// Get the search input element
+const searchInput = document.querySelector('.search');
+
+// Get the search results container element
+const searchResultsContainer = document.querySelector('#searchResults');
+
+// Function to handle the search
+function handleSearch() {
+  // Get the search query
+  const query = searchInput.value.toLowerCase();
+
+  // Perform the search logic (you can customize this based on your requirements)
+  const searchResults = performSearch(query);
+
+  // Display the search results
+  displaySearchResults(searchResults);
+}
+
+// Function to perform the search (example implementation)
+function performSearch(query) {
+  // Retrieve the words from the web page (you can modify this based on your web page structure)
+  const words = [];
+  const elements = document.querySelectorAll('.catalog, .item-1, .item-2, .item-3');
+  elements.forEach(element => {
+    const textContent = element.textContent.toLowerCase();
+    if (textContent.includes(query)) {
+      words.push({
+        word: textContent,
+        element: element
+      });
     }
   });
-});
+
+  return words;
+}
+
+// Function to display the search results
+function displaySearchResults(results) {
+  // Clear the previous search results
+  searchResultsContainer.innerHTML = '';
+
+  // Check if there are any results
+  if (results.length === 0) {
+    searchResultsContainer.textContent = 'No results found.';
+    return;
+  }
+
+  // Create and append the search result elements
+  results.forEach(result => {
+    const resultItem = document.createElement('p');
+    resultItem.textContent = result.word;
+    resultItem.classList.add('search-result');
+    resultItem.addEventListener('click', scrollToResult.bind(null, result.element));
+    searchResultsContainer.appendChild(resultItem);
+  });
+}
+
+// Function to scroll to the result and highlight the word
+function scrollToResult(element) {
+  element.classList.add('highlight');
+  element.scrollIntoView({ behavior: 'smooth' });
+}
+
+// Add event listener to the search input
+searchInput.addEventListener('input', handleSearch);
+
+
+
+
+
+
+
+
+
+
+
 
 // increasing of size of image and decreasing
 var currentPercentage = 0;
--- apps.js
+++ apps.js
@@ -1,25 +1,69 @@
-var quantity = 0;
-var totalPrice = 0;
+// Get the quantity and total price elements
+const quantityElement = document.querySelector('.quality-inside span');
+const totalPriceElement = document.querySelector('#total-price');
 
+// Set the initial quantity and price
+let quantity = 1;
+let price = 400;
+
+// Function to increase the quantity and update the total price
+function increaseQuantity() {
+  quantity++;
+  quantityElement.textContent = quantity;
+  totalPriceElement.textContent = `$${quantity * price}`;
+}
+
+// Attach event listener to the "+" button
+document.querySelector('.quality-inside button:nth-child(3)').addEventListener('click', increaseQuantity);
+
+// Function to decrease the quantity and update the total price
 function decreaseQuantity() {
-  if (quantity > 0) {
+  if (quantity > 1) {
     quantity--;
-    updateTotalPrice();
+    quantityElement.textContent = quantity;
+    totalPriceElement.textContent = `$${quantity * price}`;
   }
 }
 
-function increaseQuantity() {
-  quantity++;
-  updateTotalPrice();
+// Attach event listener to the "-" button
+document.querySelector('.quality-inside button:nth-child(1)').addEventListener('click', decreaseQuantity);
+
+
+
+
+// Get all size buttons
+const sizeButtons = document.querySelectorAll('.size-btn');
+
+// Function to handle button click
+function handleButtonClick(event) {
+  // Remove active class from all buttons
+  sizeButtons.forEach(button => {
+    button.classList.remove('active');
+  });
+
+  // Add active class to the clicked button
+  const clickedButton = event.target;
+  clickedButton.classList.add('active');
 }
 
-function updateTotalPrice() {
-  totalPrice = quantity * 400;
-  var totalPriceElement = document.getElementById("totalPrice");
-  totalPriceElement.textContent = "$" + totalPrice;
-}
+// Attach event listener to each size button
+sizeButtons.forEach(button => {
+  button.addEventListener('click', handleButtonClick);
+});
 
-updateTotalPrice(); // Set initial total price
+// Add hover effect
+sizeButtons.forEach(button => {
+  button.addEventListener('mouseenter', function() {
+    if (!this.classList.contains('active')) {
+      this.style.backgroundColor = 'blue';
+      this.style.color = 'white';
+    }
+  });
 
-var quantityElement = document.getElementById("quantity");
-quantityElement.textContent = quantity;
+  button.addEventListener('mouseleave', function() {
+    if (!this.classList.contains('active')) {
+      this.style.backgroundColor = '';
+      this.style.color = '';
+    }
+  });
+});
